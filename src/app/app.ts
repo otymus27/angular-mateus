733d6fa9a773ffb7948d8cf0a56bc24fe04a3ef8
Fábio <otymus27@gmail.com>
2025-08-08 11:53:55 -0300

    Compartilhando dados no angular

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { ComponenteFilho } from "./components/componente-filho/componente-filho";
 
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ComponenteFilho],
+  imports: [ComponenteFilho],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
+
 export class App {
-  usuario: string = 'Fábio'
+  usuario: string = 'Fábio';
+  mensageDoPai: string = 'Mensagem recebida do pai.'
 
   dados = {
     email: "otymus27@gmail.com",
     profissao: 'professor'
   }
   protected title = 'curso-mateus';
+
+  mostrarMensagem() {
+     alert(this.mensageDoPai);
+  }
 }
--- src/app/components/componente-filho/componente-filho.ts
+++ src/app/components/componente-filho/componente-filho.ts
@@ -1,4 +1,6 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {Component, Input, OnInit, Output} from '@angular/core';
+import EventEmitter = require('node:events');
+
 
 @Component({
    selector: 'app-componente-filho',
@@ -7,11 +9,21 @@ import { Component, Input, OnInit } from '@angular/core';
    styleUrl: './componente-filho.scss',
 })
 export class ComponenteFilho implements OnInit {
+   // A propriedade 'nome' pode receber dados do componente pai
    @Input() nome: string = '';
-   
+
    @Input() dados!: {email: string; profissao: string}
 
    constructor() {}
 
+   // Um alias para o @Input, com outro nome no template do pai
+   @Input() mensagemRecebida: string = '';
+
    ngOnInit(): void {}
+
+   @Output() mensagemEnviada = new EventEmitter<string>();
+
+   EnviarParaOPai(){
+      this.mensagemEnviada.emit("Estou enviando para o pai")
+   }
 }
